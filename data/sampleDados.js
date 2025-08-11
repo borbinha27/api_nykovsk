0903ebddd86b0ef3a3a3adf0ef1dccf6bb0bb0c0
Nykolas <nykolaskauansilva@gmail.com>
2025-08-11 10:41:53 -0300

    updates

---
+++ data/sampleDados.js
@@ -0,0 +1,28 @@
+const sampleDados = [
+    {
+        id: 1,
+        nome: "Nykolas Kauan",
+        idade: 18,
+        profissao: "Dev"
+    },
+    {
+        id: 2,
+        nome: "Eliel Rocha",
+        idade: 17,
+        profissao: "Dev"
+    },
+    {
+        id: 3,
+        nome: "Gustavo Mubarak",
+        idade: 17,
+        profissao: "Dev"
+    },
+    {
+        id: 4,
+        nome: "Bruno Rafael",
+        idade: 17,
+        profissao: "Dev"
+    }
+]
+
+export default sampleDados;
