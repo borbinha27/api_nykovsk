905677489caba056659d3ae7020e8b22c6770484
Arthur <arthurborba271@gmail.com>
2025-08-19 18:09:07 -0300

    se pa path arrumado

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 import express from "express";
 import dataRoutes from "./routers/router.js";
-// import cors from "cors";
-import {fileURLToPath} from "url";
-import dataRoutes from "./routers/router.js"
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
 
 const __filename = fileURLToPath(import.meta.url);
-const 
+const __dirname = dirname(__filename);
 const app = express();
 const PORT = process.env.PORT || 3000;
-app.use(cors());
 
 app.use(express.json());
+
+// Se você quiser servir arquivos estáticos, descomente a linha abaixo:
+// app.use(express.static(join(__dirname, 'public')));
 
 app.use("/", dataRoutes);
 
